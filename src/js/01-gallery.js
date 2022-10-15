25a06b6d5b673eee494aad55c64be1c79ad03c4a
Maksim6767 <nikmaksim20@gmail.com>
2022-10-15 15:10:08 +0300

    Did gallery

---
+++ src/js/01-gallery.js
@@ -0,0 +1,49 @@
+// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarkup = createGalleryImagesMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryImagesMarkup(galleryItems) {
+return galleryItems 
+.map(({ preview, original, description }) => {
+    return `
+<div class="gallery__item">
+  <a class="gallery__link" href='${original}'>
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+})
+.join('');    
+};
+
+// Реализация клика именно по картинке
+function onGalleryContainerClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+
+    console.log(event.target);
+}
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
