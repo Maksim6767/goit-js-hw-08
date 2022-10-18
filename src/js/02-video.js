b9bc2f3e93a621b49cd81773e2b9c79037318a0d
Maksim6767 <nikmaksim20@gmail.com>
2022-10-18 21:52:46 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,54 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+
+
+// function onPlay({ seconds }) {
+//    if (JSON.parse(localStorage.getItem('videoplayer-current-time')) === null) {
+//      return;
+//    }
+//   localStorage.setItem('videoplayer-current-time', seconds);
+// }
+
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+
+import '../css/common.css';
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
+
+const TIME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
+const onPlay = function (data) {
+  const strigifyData = JSON.stringify(data);
+  localStorage.setItem(TIME_KEY, strigifyData);
+};
 player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+function resumePlayback() {
+  if (JSON.parse(localStorage.getItem(TIME_KEY)) === null) {
+    return;
+  }
+  const paused = JSON.parse(localStorage.getItem(TIME_KEY))['seconds'];
+  if (paused) {
+    player
+      .setCurrentTime(paused)
+      .then(function (seconds) {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'Error':
+            break;
+          default:
+            break;
+        }
+      });
+  }
 }
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
+resumePlayback();
