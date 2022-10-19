391fdd1dc7a97fd6f38d32e313f5deebcf184afd
Maksim6767 <nikmaksim20@gmail.com>
2022-10-19 21:05:42 +0300

    For inspection

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,11 +9,10 @@ let parsedTime;
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
- localStorage.setItem('videoplayer-current-time', seconds);
  localStorage.setItem(CURRENT_TIME, seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(localStorage.getItem('CURRENT_TIME'));
 setCurrentTime();
 
 function setCurrentTime() {
