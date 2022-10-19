c209ba4d2f14b0032f284b2eb4333b6654ef9482
Maksim6767 <nikmaksim20@gmail.com>
2022-10-19 21:01:13 +0300

    Final 5

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,21 +9,20 @@ let parsedTime;
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
+ localStorage.setItem('videoplayer-current-time', seconds);
  localStorage.setItem(CURRENT_TIME, seconds);
 }
 
-player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 setCurrentTime();
 
 function setCurrentTime() {
  if (localStorage.getItem(CURRENT_TIME)) {
    parsedTime = JSON.parse(localStorage.getItem(CURRENT_TIME));
-   console.log(parsedTime);
-
+   // console.log(parsedTime);
    player.setCurrentTime(parsedTime);
  } else {
    parsedTime = 0;
-   
    player.setCurrentTime(parsedTime);
  }
 }
