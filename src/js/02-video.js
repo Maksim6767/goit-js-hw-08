2879279bbab79f25e49e1c7c3c68b73e5688cd9e
Maksim6767 <nikmaksim20@gmail.com>
2022-10-19 20:55:59 +0300

    Final 4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,6 @@
 import throttle from 'lodash.throttle';
-
 import Player from "@vimeo/player";
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const CURRENT_TIME = 'videoplayer-current-time';
@@ -9,20 +9,21 @@ let parsedTime;
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
- localStorage.setItem('videoplayer-current-time', seconds);
  localStorage.setItem(CURRENT_TIME, seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
 setCurrentTime();
 
 function setCurrentTime() {
  if (localStorage.getItem(CURRENT_TIME)) {
    parsedTime = JSON.parse(localStorage.getItem(CURRENT_TIME));
-   // console.log(parsedTime);
+   console.log(parsedTime);
+
    player.setCurrentTime(parsedTime);
  } else {
    parsedTime = 0;
+   
    player.setCurrentTime(parsedTime);
  }
 }
